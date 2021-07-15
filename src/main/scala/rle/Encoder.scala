a5d960144df22ad851abcf1400e7326fbb09bfde
AlexMontgomerie <AlexMontgomerie@github.com>
2021-07-15 17:29:38 +0100

    passing a simple stream test

--- src/main/scala/rle/Encoder.scala
+++ src/main/scala/rle/Encoder.scala
@@ -16,11 +16,11 @@ object EncoderState extends ChiselEnum {
 class Encoder[T <: UInt](gen: T, rle_zero: Int) extends Module {
 
   // get the width of the UInt type
-  val bit_width = gen.width
+  val bit_width = gen.getWidth
 
   // check the rle_zero parameter
   assert(rle_zero >= 0, "The rle zero value must be greater than zero")
-  assert(rle_zero < 256, "The rle zero value must be greater than zero")
+  assert(rle_zero < scala.math.pow(2,bit_width), "The rle zero value must be greater than zero")
 
   // intialise io definition
   val io = IO(new EncoderIO(gen))
@@ -29,55 +29,56 @@ class Encoder[T <: UInt](gen: T, rle_zero: Int) extends Module {
   val state = RegInit(EncoderState.EMPTY)
 
   // create registers to store incoming values and the rle counter
-  val rle_cntr = RegInit(0.U)
+  val rle_cntr = RegInit(0.U(bit_width.W))
+
+  // set defaults
+  io.out.bits := DontCare
+  io.out.valid := false.B
+  io.in.ready := false.B
 
   // create the finite state machine
   switch (state) {
     is(EncoderState.EMPTY) {
       when (io.in.bits =/= rle_zero.U && io.in.valid) {
         // pass value straight through if it's not an rle zero
-        io.out.bits   := io.in.bits
-        io.out.valid  := io.in.valid
         state := EncoderState.EMPTY
       } .elsewhen ( io.in.bits === rle_zero.U && io.in.valid ) {
         // move to the FILL state if the incoming value is an rle
         // zero, and update the rle counter
-        io.out.bits   := DontCare
-        io.out.valid  := false.B
-        state := EncoderState.Fill
+        state := EncoderState.FILL
         rle_cntr := rle_cntr + 1.U
-      } .otherwise {
-        // otherwise, stay in the EMPTY state
-        io.out.bits   := DontCare
-        io.out.valid  := false.B
-        state := EncoderState.EMPTY
       }
-      // connect the ready signal straight through
-      io.in.ready := io.out.ready
+      // connect the ready signal straight through, and the
+      // input directly to output
+      io.out.bits   := io.in.bits
+      io.out.valid  := io.in.valid
+      io.in.ready   := io.out.ready
     }
     is(EncoderState.FILL) {
       when (io.in.bits =/= rle_zero.U && io.in.valid) {
         // go the the DUMP state, and cache the last value
-        io.out.bits   := DontCare
-        io.out.valid  := false.B
         io.in.ready   := false.B
-        state := DUMP
+        state := EncoderState.DUMP
       } .elsewhen ( io.in.bits === rle_zero.U && io.in.valid ) {
         // keep in current state, and increment the rle counter
-        io.out.bits   := DontCare
-        io.out.valid  := false.B
         io.in.ready   := true.B
-        state := EncoderState.Fill
+        state := EncoderState.FILL
         rle_cntr := rle_cntr + 1.U
-      } .otherwise {
-        // stay in the FILL state
       }
-    }
+      // no output during the FILL stage
+      io.out.bits   := DontCare
+      io.out.valid  := false.B
+   }
     is(EncoderState.DUMP) {
-
+      // pass the total number of zeros to the output, and
+      // go to the output state
+      io.out.bits := rle_cntr
+      io.out.valid := true.B
+      io.in.ready := false.B
+      state := EncoderState.EMPTY
     }
     is(EncoderState.OVERFLOW) {
-
+      // TODO: handle case
     }
   }
 
--- src/test/scala/rle/EncoderUnitTests.scala
+++ src/test/scala/rle/EncoderUnitTests.scala
@@ -1,5 +1,7 @@
 package rle.encoder_test
 
+import rle.Encoder
+
 import org.scalatest._
 
 import chisel3._
@@ -18,7 +20,7 @@ class EncoderTest extends FlatSpec with ChiselScalatestTester with Matchers {
   behavior of "Encoder"
   it should "be correct for a stream with no zeros" in {
     // create the DUT
-    test(new Encoder(UInt(8.W), 0.U)).withAnnotations(annotations) { c =>
+    test(new Encoder(UInt(8.W), 0)).withAnnotations(annotations) { c =>
 
       // create test sequences
       val seq_in = Seq(1.U, 2.U, 3.U, 4.U, 5.U, 6.U, 7.U, 8.U, 9.U)
@@ -30,12 +32,35 @@ class EncoderTest extends FlatSpec with ChiselScalatestTester with Matchers {
 
       // run the sequences
       parallel(
-        c.io.in.enqueue(seq_in),
-        c.io.in.enqueue(seq_out)
+        c.io.in.enqueueSeq(seq_in),
+        c.io.out.expectDequeueSeq(seq_out)
       )
 
     }
   }
+
+  behavior of "Encoder"
+  it should "be correct for a stream with both zeros and non-zeros" in {
+    // create the DUT
+    test(new Encoder(UInt(8.W), 0)).withAnnotations(annotations) { c =>
+
+      // create test sequences
+      val seq_in = Seq(1.U, 2.U, 0.U, 0.U, 0.U, 0.U, 7.U, 8.U, 9.U)
+      val seq_out = Seq(1.U, 2.U, 0.U, 4.U, 7.U, 8.U, 9.U)
+
+      // module setup
+      c.io.in.initSource().setSourceClock(c.clock)
+      c.io.out.initSink().setSinkClock(c.clock)
+
+      // run the sequences
+      parallel(
+        c.io.in.enqueueSeq(seq_in),
+        c.io.out.expectDequeueSeq(seq_out)
+      )
+
+    }
+  }
+
 
 }
 
